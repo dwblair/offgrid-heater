eca4581b5ab7d16ff83ed338adf40f9d86760338
Don Blair <donblair@gmail.com>
2021-11-22 17:47:59 -0500

    update

--- v2/firmware/plotter_min.m
+++ v2/firmware/plotter_min.m
@@ -1,7 +1,10 @@
 set datafile separator ","
-set xlabel "minutes"
-set ylabel "deg F"
-plot "output.txt" using ($1/60):($3*9/5+32)
+set ytics 20 nomirror tc lt 1
+set ylabel 'pwm (%)' tc lt 1
+set y2tics 10 nomirror tc lt 2
+set y2label 'temp (deg F)' tc lt 2
+set xlabel 'minutes'
+plot "output.txt" using ($1/60):($2/255*100) linetype 1 title "pwm", "output.txt" using ($1/60):($3*9/5+32) linetype 2 axes x1y2 title"temp"
 pause 5 
 reread
 
