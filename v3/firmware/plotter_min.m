0a03349e06f7cc758279cc19b25b4ab46912ef6e
Don Blair <donblair@gmail.com>
2021-11-23 12:49:21 -0500

    update

--- v3/firmware/plotter_min.m
+++ v3/firmware/plotter_min.m
@@ -4,6 +4,7 @@ set ylabel 'pwm (%)' tc lt 1
 set y2tics 10 nomirror tc lt 2
 set y2label 'temp (deg F)' tc lt 2
 set xlabel 'minutes'
+set title "1 heater; batt_initial: 12.0V"
 plot "output.txt" using ($1/60):($2/255*100) linetype 1 title "pwm", "output.txt" using ($1/60):($3*9/5+32) linetype 2 axes x1y2 title"temp"
 pause 5 
 reread
